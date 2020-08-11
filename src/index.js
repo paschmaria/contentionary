d19696c82944bb4dd2e6770ba4cd28f3fc2b4fa3
Paschal Onuorah <onuorahpaschal@gmail.com>
2020-08-11 10:37:30 +0100

    Minor changes

--- src/index.js
+++ src/index.js
@@ -16,12 +16,12 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(reducers, composeEnhancers(applyMiddleware(thunk)));
 
 ReactDOM.render(
-    <Router>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </Router>,
-    document.getElementById('root'));
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
